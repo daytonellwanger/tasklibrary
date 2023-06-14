875acae7c511dc10dafd9b754653738bb6b6adfc
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-13 18:04:56 -0700

    Update how task is scheduled

--- install-spotify/install.ps1
+++ install-spotify/install.ps1
@@ -1,3 +1,17 @@
-$Trigger = New-ScheduledTaskTrigger -AtLogon
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; choco install spotify -y"
-Register-ScheduledTask -TaskName "Customization" -Trigger $Trigger -Action $Action -RunLevel Highest -Force
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = 'Customizations'
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $false
+
+$trigger = $task.triggers.Create(9)
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'PowerShell.exe'
+$action.Arguments = "Set-ExecutionPolicy Bypass -Scope Process -Force; choco install spotify -y"
+
+$taskFolder = $ShedService.GetFolder("\")
+$taskFolder.RegisterTaskDefinition('Customizations', $Task , 6, 'Users', $null, 4)
