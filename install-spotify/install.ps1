78cccdf5b62abb8011469be8dbdd7b8f5224ef1c
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-14 14:01:04 -0700

    Fix cleanup

--- install-spotify/install.ps1
+++ install-spotify/install.ps1
@@ -6,16 +6,28 @@ if (!(Test-Path -PathType Container "C:\DevBoxCustomizations")) {
     Copy-Item "./cleanup.ps1" -Destination "C:\DevBoxCustomizations"
 }
 
-# Schedule the cleanup script to run every minute
-$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 1)
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\DevBoxCustomizations\cleanup.ps1"
-Register-ScheduledTask -TaskName "CustomizationsCleanup" -Trigger $Trigger -Action $Action -RunLevel Highest -Force
-
-# Schedule the script to be run in the user context on login
 # Reference: https://learn.microsoft.com/en-us/windows/win32/taskschd/task-scheduler-objects
 $ShedService = New-Object -comobject "Schedule.Service"
 $ShedService.Connect()
 
+# Schedule the cleanup script to run every minute as the SYSTEM
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = "Customizations cleanup"
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $false
+
+$Trigger = $Task.Triggers.Create(9)
+$Trigger.Enabled = $true
+$Trigger.Repetition.Interval="PT1M"
+
+$Action = $Task.Actions.Create(0)
+$Action.Path = "PowerShell.exe"
+$Action.Arguments = "Set-ExecutionPolicy Bypass -Scope Process -Force; C:\DevBoxCustomizations\cleanup.ps1"
+
+$TaskFolder = $ShedService.GetFolder("\")
+$TaskFolder.RegisterTaskDefinition("CustomizationsCleanup", $Task , 6, "NT AUTHORITY\SYSTEM", $null, 5)
+
+# Schedule the script to be run in the user context on login
 $Task = $ShedService.NewTask(0)
 $Task.RegistrationInfo.Description = "Customizations"
 $Task.Settings.Enabled = $true
@@ -26,7 +38,7 @@ $Trigger.Enabled = $true
 
 $Action = $Task.Actions.Create(0)
 $Action.Path = "PowerShell.exe"
-$Action.Arguments = "C:\DevBoxCustomizations\runonce.ps1"
+$Action.Arguments = "Set-ExecutionPolicy Bypass -Scope Process -Force; C:\DevBoxCustomizations\runonce.ps1"
 
 $TaskFolder = $ShedService.GetFolder("\")
 $TaskFolder.RegisterTaskDefinition("Customizations", $Task , 6, "Users", $null, 4)
