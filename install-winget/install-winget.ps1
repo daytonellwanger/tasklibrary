1ae737c552c2fa6eebb4c20d2bc136761504a551
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-16 11:30:20 -0700

    Install winget for all users

--- install-winget/install-winget.ps1
+++ install-winget/install-winget.ps1
@@ -1,5 +1,5 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module Microsoft.WinGet.Client
+Install-Module Microsoft.WinGet.Client -Scope AllUsers
 
 echo "Repair-WinGetPackageManager" > "C:\DevBoxCustomizations\repairwinget.ps1"
