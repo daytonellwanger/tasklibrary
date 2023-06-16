7e49796b94d073491519f77230310ce67153f9ba
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-16 16:25:15 -0700

    Don't use winget to install ps7

--- install-ps7/install.ps1
+++ install-ps7/install.ps1
@@ -1,1 +1,3 @@
-winget install --id Microsoft.Powershell --source winget
+$code = Invoke-RestMethod -Uri https://aka.ms/install-powershell.ps1
+$null = New-Item -Path function:Install-PowerShell -Value $code
+Install-PowerShell -UseMSI -Quiet
