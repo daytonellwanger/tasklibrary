e9ac6fdf1a3d14cb1759753b38b1f1047965f64d
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-21 13:47:16 -0700

    Install winget configure too

--- install-winget/install-winget.ps1
+++ install-winget/install-winget.ps1
@@ -1,6 +1,6 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module Microsoft.WinGet.Client -Scope AllUsers
+Install-Module Microsoft.WinGet.Configuration -AllowPrerelease -Scope AllUsers
 Repair-WinGetPackageManager -Latest
-
 Add-Content -Path "C:\DevBoxCustomizations\runAsUser.ps1" -Value "Repair-WinGetPackageManager -Latest"
