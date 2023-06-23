6386f19b60527be572809d8b7d5a21b281ae4066
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-23 10:03:08 -0700

    Debugging

--- winget/install.ps1
+++ winget/install.ps1
@@ -4,5 +4,5 @@ param(
  )
 
 # Run in both system context and user context
-pwsh.exe -MTA -Command "Install-WinGetPackage -Id $($Package)"
-Add-Content -Path "C:\DevBoxCustomizations\runAsUser.ps1" -Value "Install-WinGetPackage -Id $($Package)"
+pwsh.exe -MTA -Command "Start-Transcript -path C:\manualinstall.txt -append; Install-WinGetPackage -Id $($Package); Stop-Transcript"
+# Add-Content -Path "C:\DevBoxCustomizations\runAsUser.ps1" -Value "Install-WinGetPackage -Id $($Package)"
