b40055dfcaf2149ed5bca6439955933bd617bd12
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-23 14:59:39 -0700

    Move transcript outside of pwsh

--- winget/install.ps1
+++ winget/install.ps1
@@ -4,5 +4,7 @@ param(
  )
 
 # Run in both system context and user context
-pwsh.exe -MTA -Command "Start-Transcript -path C:\manualinstall.txt -append; Install-WinGetPackage -Id $($Package); Stop-Transcript"
+Start-Transcript -path C:\manualinstall.txt -append
+pwsh.exe -MTA -Command "Install-WinGetPackage -Id $($Package)"
+Stop-Transcript
 # Add-Content -Path "C:\DevBoxCustomizations\runAsUser.ps1" -Value "Install-WinGetPackage -Id $($Package)"
