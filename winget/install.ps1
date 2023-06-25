5a06ffa178437bf8e05a82aa423b583476edebda
Dayton Ellwanger <daellwan@microsoft.com>
2023-06-25 08:44:39 -0700

    New technique for launching new process

--- winget/install.ps1
+++ winget/install.ps1
@@ -8,10 +8,11 @@ Start-Transcript -path C:\manualinstall.txt -append
 pwsh.exe -MTA -Command "Install-WinGetPackage -Id $($Package)" > C:\initialinstalltest.txt
 Stop-Transcript
 
-Start-Process pwsh.exe -ArgumentList "-MTA -Command `"Install-WinGetPackage -Id Notepad++.Notepad++ > C:\newprocesstest.txt`""
+Invoke-CimMethod -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine='pwsh.exe -MTA -Command "Install-WinGetPackage -Id Notepad++.Notepad++ > C:\newprocesstest.txt"'}
+
 # Restart-Computer
 # Add-Content -Path "C:\DevBoxCustomizations\runAsUser.ps1" -Value "Install-WinGetPackage -Id $($Package)"
 
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(2)
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "pwsh.exe -MTA -Command C:\DevBoxCustomizations\runassystem.ps1"
-Register-ScheduledTask -TaskName "CustomizationTest" -Trigger $Trigger -Action $Action -User System -RunLevel Highest -Force
+# Register-ScheduledTask -TaskName "CustomizationTest" -Trigger $Trigger -Action $Action -User System -RunLevel Highest -Force
