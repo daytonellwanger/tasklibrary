a85b1eb781ae116801c7e423be6251a059bd9c61
Dayton Ellwanger <daellwan@microsoft.com>
2023-07-11 16:54:06 -0700

    Wait on OneDrive init

--- winget/runAsUser.ps1
+++ winget/runAsUser.ps1
@@ -6,4 +6,6 @@ $CleanupScript = "cleanup.ps1"
 $RunAsUserTask = "DevBoxCustomizations"
 $CleanupTask = "DevBoxCustomizationsCleanup"
 
+echo "Waiting on OneDrive initialization..."
+Start-Sleep -Seconds 120
 Remove-Item -Path "$($CustomizationScriptsDir)\$($LockFile)"
